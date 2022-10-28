6979dce6d3691c382d51d5654fecfedce64dd0f0
RHN007 <rakibul.nayon@outlook.com>
2022-10-28 10:31:33 +0600

    finalizing server site

--- index.js
+++ index.js
@@ -9,12 +9,37 @@ app.get('/', (req, res) => {
     res.send('Hello World')
   })
   
-
 const categories = require('./data/categories.json'); 
+const courses = require('./data/courses.json')
+
+
 app.get('/categories', (req, res)=> {
     res.send(categories)
+    console.log(categories)
 })
 
+app.get('/category/:id', (req, res)=> {
+    const id = req.params.id; 
+    if(id === "08"){
+        res.send(courses)
+    }
+    else {
+        const category_courses = courses.filter(c => c.category_id ===id)
+        res.send(category_courses)
+    }
+   
+})
+
+app.get('/courses', (req, res)=> {
+    res.send(courses)
+})
+
+app.get('/courses/:id', (req, res)=> {
+  console.log(req.params.id)
+  const id = req.params.id
+  const selected_course = courses.find(n => n._id === id)
+  res.send(selected_course)
+})
 
 
 app.listen(port, () => {
